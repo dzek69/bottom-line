e198b4be82555fd259093fce3e7eed05d5a43dfc
Jacek Nowacki <git-public@dzek.eu>
2018-05-31 01:09:47 +0200

    - added global index file with all the methods

---
+++ src/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    get: require("./get"),
+    getMultiple: require("./getMultiple"),
+    isEmpty: require("./isEmpty"),
+    last: require("./last"),
+    mapValues: require("./mapValues"),
+};

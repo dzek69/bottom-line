f4c9f3d8af28b751cd6e09c5ebee3b3b160de696
Jacek Nowacki <git-public@dzek.eu>
2018-05-30 23:21:53 +0200

    - docs for `getMultiple` added

--- src/getMultiple.js
+++ src/getMultiple.js
@@ -1,6 +1,21 @@
 const get = require("./get");
 
 const DEFAULT = {};
+/**
+ * Returns first found value at given list of paths of given object. Will return and stop at undefined if found. If
+ * nothing is found then default value (required to pass) will be returned.
+ *
+ * @param {Object} source - source object to search in
+ * @param {*} defaultValue - default value to return if nothing is found
+ * @param {...string|Array.<string>} paths - paths defined as dot-separated properties names or array of properties name
+ * @see {@link get} - for base usage example with single path only
+ * @example getMultiple(obj, 5, "details.error.message", ["error", "message"], "errorMessage")
+ * // will look for obj.details.error.message - if path does not exist
+ * // will look for obj.error.message - if not defined
+ * // will look for obj.errorMessage - if not defined
+ * // will return 5
+ * @returns {*} - found value or default value
+ */
 const getMultiple = (source, defaultValue, ... paths) => {
     const length = paths.length;
     for (let i = 0; i < length; i++) {

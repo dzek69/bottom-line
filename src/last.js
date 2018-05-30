f6650a84287e677d0ecd85e1e31a4e27e81ec5fd
Jacek Nowacki <git-public@dzek.eu>
2018-05-30 23:41:20 +0200

    - docs for `last` added

--- src/last.js
+++ src/last.js
@@ -1,3 +1,12 @@
+/**
+ * Gets last element of an array. Will crash on non-array like values.
+ *
+ * @param {Array} array - source array
+ * @example last([1, 2]) // 2
+ * @example last([1]) // 1
+ * @example last([]) // undefined
+ * @returns {*} - last element of an array or undefined
+ */
 const last = array => array[array.length - 1];
 
 module.exports = last;
